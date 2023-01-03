4da007bb1040a912e8eefe66a551d0a7d35f6bb3
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-03 16:05:28 -0300

    Exercicio 3 - Desafio 1

--- exercicio-3/desafio-1/Program.cs
+++ exercicio-3/desafio-1/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("========= Exercício 3 - Desafio 1 =========");
+
+var input = File.ReadAllLines("input.txt");
+
+var prioridade = 0;
+
+foreach(var line in input)
+{
+    var listaItens = new List<char>();
+
+    var primeiroCompartimento = line.Substring(0, (int)(line.Length / 2));
+    var segundoCompartimento  = line.Substring((int)(line.Length / 2), (int)(line.Length / 2));
+
+    foreach(var item1 in primeiroCompartimento)
+    {
+        if(listaItens.Contains(item1))
+            continue;
+
+        foreach(var item2 in segundoCompartimento)
+        {
+            if (item1 == item2)
+            {
+                if(listaItens.Contains(item2))
+                    continue;
+
+                listaItens.Add(item1);
+            }
+        }
+    }
+
+    foreach(var item in listaItens)
+    {
+        prioridade += ValorPrioridade(item);
+    }
+}
+
+Console.WriteLine("Prioridade dos itens repetidos é: " + prioridade);
+
+int ValorPrioridade(char item)
+{
+    if(isMinusculo(item))
+        return (int)(item - 'a')+1;
+
+    if(isMaiusculo(item))
+        return (int)(item - 'A')+27;
+
+    return 0;
+}
+
+bool isMinusculo(char ch) => ch >= 'a' && ch <= 'z';
+bool isMaiusculo(char ch) => ch >= 'A' && ch <= 'Z';
