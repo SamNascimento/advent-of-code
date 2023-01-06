0b8a51cc21edd181e000b58d450bff8875897001
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-06 12:00:00 -0300

    Concluindo o exercício 5 - desafio 2

--- exercicio-5/desafio-2/Program.cs
+++ exercicio-5/desafio-2/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("========= Exercício 5 - Desafio 2 =========");
+
+// var input      = File.ReadAllText("test.txt");
+var input      = File.ReadAllText("input.txt");
+var cleanInput = input.Replace("\r", "");
+
+var dividedInput = cleanInput.Split("\n\n");
+
+var topCrate = new List<char>();
+
+var inputCrates = dividedInput[0].Split('\n');
+var commands    = dividedInput[1].Split('\n');
+
+var cratesSize = (inputCrates[inputCrates.Length - 1].Length + 1) / 4;
+
+var crates = new List<char>[cratesSize];
+
+for (var i = 0; i < crates.Length; i++)
+{
+    crates[i] = new List<char>();
+}
+
+for (var i = inputCrates.Length - 2; i >= 0 ; i--)
+{
+    for (var cratesIndex = 0; cratesIndex < crates.Length; cratesIndex++)
+    {
+        var index = (4 * cratesIndex) + 1; 
+        var letra = inputCrates[i][index];
+
+        if(letra != ' ')
+            crates[cratesIndex].Add(letra);
+    }
+}
+
+foreach (var command in commands)
+{
+    var lines = command.Split(' ');
+
+    var moveNumber = int.Parse(lines[1]);
+    var fromNumber = int.Parse(lines[3]) - 1;
+    var toNumber   = int.Parse(lines[5]) - 1;
+
+    var charMoved = crates[fromNumber].TakeLast(moveNumber).ToArray();
+
+    crates[toNumber].AddRange(charMoved);
+    
+    for(var i = 0; i < charMoved.Length; i++)
+    {
+        crates[fromNumber].RemoveAt(crates[fromNumber].Count - 1);
+    }
+}
+
+foreach (var crate in crates)
+{
+    topCrate.Add(crate.Last());
+}
+
+Console.WriteLine(new string(topCrate.ToArray()));
