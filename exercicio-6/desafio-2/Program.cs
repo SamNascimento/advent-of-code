32fbd9a8bf7f09fa7c7994dd51df99a9ca4ed213
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-10 13:02:32 -0300

    Concluindo exercício 6 - desafio 2

--- exercicio-6/desafio-2/Program.cs
+++ exercicio-6/desafio-2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("========= Exercício 6 - Desafio 2 =========");                  
+ 
+// var input      = File.ReadAllText("test-1.txt");
+var input      = File.ReadAllText("input.txt");
+var cleanInput = input.Replace("\r", "");
+
+var indexOfStart = 0;
+
+for (var i = 13; i < input.Length; i++)
+{
+    var hashString = input.Substring(i-13, 14);
+    var sizeOfHash = hashString.Distinct().Count();
+
+    if (sizeOfHash == 14)
+    {
+        indexOfStart = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine("The start-of-packet index is: " + indexOfStart);
