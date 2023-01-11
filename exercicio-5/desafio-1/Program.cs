fa406334c4ddb6ae9d274501edba8c0b0a1e4f13
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-10 22:43:07 -0300

    Melhorando exercício-5

--- exercicio-5/desafio-1/Program.cs
+++ exercicio-5/desafio-1/Program.cs
@@ -10,15 +10,14 @@ var topCrate = new List<char>();
 var inputCrates = dividedInput[0].Split('\n');
 var commands    = dividedInput[1].Split('\n');
 
-var crates = new Stack<char>[9];
-//var crates = new Stack<char>[3];
+var cratesSize = (inputCrates[inputCrates.Length - 1].Length + 1) / 4;
+
+var crates = new Stack<char>[cratesSize];
 
 for (var i = 0; i < crates.Length; i++)
 {
     crates[i] = new Stack<char>();
 }
-
-//int cratesIndex = 0;
 
 for (var i = inputCrates.Length - 2; i >= 0; i--)
 {
