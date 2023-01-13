0cddd251d9a0093cc45ced92451f24014829c3fd
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-13 18:53:16 -0300

    Finalizando exercício 9 - Desafio 1

--- exercicio-9/desafio-1/Program.cs
+++ exercicio-9/desafio-1/Program.cs
@@ -1,144 +1,128 @@
 ﻿Console.WriteLine("========= Exercício 9 - Desafio 1 =========");                  
  
-// var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt");
+// var input = File.ReadAllLines("test.txt");
 
-// var x = 1000;
-// var y = 1000;
+var head      = new Position(0, 0);
+var tailsList = new List<Position>(); 
 
-// var initialX = 500;
-// var initialY = 500;
+tailsList.Add(new Position(0, 0));
 
-var input = File.ReadAllLines("test.txt");
-
-var x = 6;
-var y = 5;
-
-var initialX = 0;
-var initialY = y - 1;
-
-var actualX = initialX;
-var actualY = initialY;
-
-var planck = InicializePlanck(x, y);
-
-planck[initialX, initialY] = 'H';
-
-foreach (var line in input)
+foreach(var line in input)
 {
-    var dividedLine  = line.Split(' ');
-    var direction    = dividedLine[0];
-    var numMovements = int.Parse(dividedLine[1]);
-    var breakNumber  = 0;
+    var lineSplited = line.Split(' ');
+    var direction   = lineSplited[0];
+    var numSteps    = int.Parse(lineSplited[1]);
 
-    switch (direction)
+    for (var i = 0; i < numSteps; i++)
     {
-        case "R":
-            breakNumber = actualX + numMovements;
-            for (var i = actualX; i <= breakNumber; i++) 
-            {
-                actualX = i;
+        head = MoveHead(head, direction);
+        tailsList.Add(MoveTail(head, tailsList.Last()));
+    }
+}
 
-                if (actualX >= planck.GetLength(0))
-                    throw new Exception($"Moviment out of bound while tryied to move \"{direction}\" to {numMovements} steps");
-                else
-                    planck[actualX, actualY] = 'H';
-            }
-            break;
-        case "L":
-            breakNumber = actualX - numMovements;
-            for (var i = actualX; i >= breakNumber; i--)
-            {
-                actualX = i;
-                
-                if (actualX < 0)
-                    throw new Exception($"Moviment out of bound while tryied to move \"{direction}\" to {numMovements} steps");
-                else
-                    planck[actualX, actualY] = 'H';
-            }
-            break;
+var uniqueListOfTails = new List<Position>();
+
+foreach (var tail in tailsList)
+    uniqueListOfTails = UniqueLocations(uniqueListOfTails, tail);
+
+var countUniqueLocationsTail = uniqueListOfTails.Count();
+
+Console.WriteLine(countUniqueLocationsTail);
+
+#region Methods
+
+Position MoveHead(Position head, string moviment)
+{
+    switch (moviment)
+    {
         case "U":
-            breakNumber = actualY - numMovements;
-            for (var j = actualY; j >= breakNumber; j--)
-            {
-                actualY = j;
-
-                if (actualY < 0)
-                    throw new Exception($"Moviment out of bound while tryied to move \"{direction}\" to {numMovements} steps");
-                else
-                    planck[actualX, actualY] = 'H';
-            }
+            head.y += 1;
             break;
         case "D":
-            breakNumber = actualY + numMovements;
-            for (var j = actualY; j <= breakNumber; j++)
-            {
-                actualY = j;
-
-                if (actualY >= planck.GetLength(1))
-                    throw new Exception($"Moviment out of bound while tryied to move \"{direction}\" to {numMovements} steps");
-                else
-                    planck[actualX, actualY] = 'H';
-            }
+            head.y -= 1;
+            break;
+        case "L":
+            head.x -= 1;
+            break;
+        case "R":
+            head.x += 1;
             break;
         default:
             break;
     }
+
+    return head;
 }
 
-var totalMovedPlancks = CountMovedPlanck(x, y, planck);
-
-var textPlanck = DrawPlanck(x, y, planck);
-
-Console.WriteLine(textPlanck);
-Console.WriteLine("Total moved plancks is: " + totalMovedPlancks);
-
-#region Methods
-
-char[,] InicializePlanck(int x, int y)
+Position MoveTail(Position head, Position tail)
 {
-    var planck = new char[x, y];
+    if (tail.x == head.x && tail.y == head.y)
+        return tail;
 
-    for (var i = 0; i < y; i++)
+    var newTail = new Position(tail.x, tail.y);
+    
+    if (head.x > newTail.x + 1)
     {
-        for (var j = 0; j < x; j++)
-        {
-            planck[j, i] = 'S';
-        }
+        if (head.y > newTail.y)
+            newTail.y += 1;
+        else if (head.y < newTail.y)
+            newTail.y -= 1;
+
+        newTail.x += 1;
+    }    
+    else if (head.x < newTail.x - 1)
+    {
+        if (head.y > newTail.y)
+            newTail.y += 1;
+        else if (head.y < newTail.y)
+            newTail.y -= 1;
+
+        newTail.x -= 1;
+    }
+    else if (head.y > newTail.y + 1)
+    {
+        if (head.x > newTail.x)
+            newTail.x += 1;
+        else if (head.x < newTail.x)
+            newTail.x -= 1;
+
+        newTail.y += 1;
+    }    
+    else if (head.y < newTail.y - 1)
+    {
+        if (head.x > newTail.x)
+            newTail.x += 1;
+        else if (head.x < newTail.x)
+            newTail.x -= 1;
+
+        newTail.y -= 1;
     }
 
-    return planck;
+    return newTail;
 }
 
-int CountMovedPlanck(int x, int y, char[,] planck)
+List<Position> UniqueLocations(List<Position> listTails, Position currentTail)
 {
-    var count = 0;
+    var haveSameTail = listTails.Where(l => l.x == currentTail.x && l.y == currentTail.y).Any();
 
-    for (var i = 0; i < y; i++)
-    {
-        for (var j = 0; j < x; j++)
-        {
-            if (planck[j, i] != 'S')
-                count += 1;
-        }
-    }
+    if (!haveSameTail)
+        listTails.Add(currentTail);
 
-    return count;
+    return listTails;
 }
+#endregion
 
-string DrawPlanck(int x, int y, char[,] planck)
+#region Class
+public class Position
 {
-    var textPlanck = "";
+    public int x {get; set;}
+    public int y {get; set;}
 
-    for (var i = 0; i < y; i++)
+    public Position(int x, int y)
     {
-        for (var j = 0; j < x; j++)
-        {
-            textPlanck += $" {planck[j, i]} ";
-        }
-        textPlanck += '\n';
+        this.x    = x;
+        this.y    = y;
     }
-
-    return textPlanck;
 }
-
 #endregion
