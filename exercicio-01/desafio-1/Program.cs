e24acb88b8f9ba0379c5900eb9e7f4b41ea3a465
Samuel Nascimento <samuelpn15@gmail.com>
2023-01-17 14:43:34 -0300

    Trocando nomenclatura das pastas e adicionando o exercício 1

---
+++ exercicio-01/desafio-1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("========= Exercício 1 - Desafio 1 =========");                  
+ 
+var input = File.ReadAllText("input.txt");
+// var input = File.ReadAllText("test.txt");
+
+var listElfsBackpacks = input.Split("\n\n");
+
+var listElfs = new List<Elf>();
+
+foreach (var elfBackpack in listElfsBackpacks)
+{
+    var listItens = elfBackpack.Split('\n');
+    var calories  = listItens.Select(l => int.Parse(l));
+
+    var elf = new Elf(calories);
+    listElfs.Add(elf);
+}
+
+var topCalorie = listElfs.Max(l => l.SumCalories);
+
+Console.WriteLine("The max calorie number carrying by an elf is: " + topCalorie);
+
+#region Classes
+class Elf
+{
+    public IEnumerable<int> ItensCalories { get; set; }
+    public long SumCalories { get; set; }
+
+    public Elf(IEnumerable<int> itens)
+    {
+        ItensCalories = itens;
+        SumCalories   = itens.Sum();
+    }
+}
+#endregion
---
+++ exercicio-01/desafio-2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("========= Exercício 1 - Desafio 2 =========");                  
+ 
+var input = File.ReadAllText("input.txt");
+// var input = File.ReadAllText("test.txt");
+
+var listElfsBackpacks = input.Split("\n\n");
+
+var listElfs = new List<Elf>();
+
+foreach (var elfBackpack in listElfsBackpacks)
+{
+    var listItens = elfBackpack.Split('\n');
+    var calories  = listItens.Select(l => int.Parse(l));
+
+    var elf = new Elf(calories);
+    listElfs.Add(elf);
+}
+
+var topCalories = listElfs.Select(l => l.SumCalories).OrderByDescending(l => l).Take(3);
+
+Console.WriteLine("The top three calories number carrying by an elf is: ");
+foreach (var calories in topCalories)
+    Console.WriteLine(calories);
+Console.WriteLine("And the sum of that calories is: " + topCalories.Sum());
+
+#region Classes
+class Elf
+{
+    public IEnumerable<int> ItensCalories { get; set; }
+    public long SumCalories { get; set; }
+
+    public Elf(IEnumerable<int> itens)
+    {
+        ItensCalories = itens;
+        SumCalories   = itens.Sum();
+    }
+}
+#endregion
