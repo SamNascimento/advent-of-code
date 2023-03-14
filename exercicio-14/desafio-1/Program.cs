6d4be7c5c4303c1830abb9d567c6f52e64e0854c
Samuel Nascimento <samuel.pn15@gmail.com>
2023-03-14 17:02:53 -0300

    iniciando exercício 14

---
+++ exercicio-14/desafio-1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("========= Exercício 14 - Desafio 1 =========");                  
+ 
+// var input = File.ReadAllLines("test.txt");
+var input = File.ReadAllLines("input.txt");
---
+++ exercicio-14/desafio-2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("========= Exercício 14 - Desafio 2 =========");                  
+ 
+// var input = File.ReadAllLines("test.txt");
+var input = File.ReadAllLines("input.txt");
