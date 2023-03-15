89bbc8a1b19f78d4c1f886d12f5b0a44a88f73cb
Samuel Nascimento <samuel.pn15@gmail.com>
2023-03-15 18:38:37 -0300

    Trabalhando melhor o esboço de implementação

--- exercicio-14/desafio-1/Program.cs
+++ exercicio-14/desafio-1/Program.cs
@@ -7,11 +7,28 @@ var map = new CaveMap();
 
 foreach (var line in input)
 {
-    
+    var coordenates = line.Split("->");
+
+    for (var i = 0; i < coordenates.Length - 1; i++)
+    {
+        var coordBegin = coordenates[i];
+        var coordEnd   = coordenates[i+1];
+
+        map = ConstructMap(map, coordBegin, coordEnd);
+    }
 }
 
+var minX = map.nodes.Select(n => n.x).Min();
+var maxX = map.nodes.Select(n => n.x).Max();
+var minY = 0;
+var maxY = map.nodes.Select(n => n.y).Max();
+
+map = FillAirInMap(map, minX, maxX, minY, maxY);
+
+
+
 #region Methods
-public void ConstructMap (CaveMap map, string begin, string end)
+CaveMap ConstructMap (CaveMap map, string begin, string end)
 {
     var coordBegin = begin.Trim().Split(',');
     var coordEnd   = end.Trim().Split(',');
@@ -28,8 +45,119 @@ public void ConstructMap (CaveMap map, string begin, string end)
         {
             for (int i = beginX; i <= endX; i++)
             {
-                var newNode = new Node(i, endY, '#');
+                var newNode = new Node(i, beginY, '#');
+                map.nodes.Add(newNode);
             }
+        }
+        else 
+        {
+            for (int i = endX; i <= beginX; i++)
+            {
+                var newNode = new Node(i, beginY, '#');
+                map.nodes.Add(newNode);
+            }
+        }
+    }
+
+    if (beginY != endY)
+    {
+        if (beginY < endY)
+        {
+            for (int i = beginY; i <= endY; i++)
+            {
+                var newNode = new Node(beginX, i, '#');
+                map.nodes.Add(newNode);
+            }
+        }
+        else 
+        {
+            for (int i = endY; i <= beginY; i++)
+            {
+                var newNode = new Node(beginX, i, '#');
+                map.nodes.Add(newNode);
+            }
+        }
+    }
+
+    return map;
+}
+
+CaveMap FillAirInMap (CaveMap map, int minX, int maxX, int minY, int maxY)
+{
+    for (int i = minX; i <= maxX; i++)
+    {
+        for (int j = minY; j <= maxY; j++)
+        {
+            var actualNode = map.nodes.Where(n => n.x == i && n.y == j).FirstOrDefault();
+
+            if (actualNode == null)
+            {
+                var newNode = new Node(i, j, '.');
+                map.nodes.Add(newNode);
+            }
+        }
+    }
+
+    return map;
+}
+
+int SandResting(CaveMap map)
+{
+    var isFinished = false;
+
+    while(!isFinished)
+        (isFinished, map) = DropSand(map);
+
+    return map.nodes.Where(n => n.obj == 'o').Count();
+}
+
+(bool isFinished, CaveMap mapUpdated) DropSand(CaveMap map)
+{
+    var actualX = 500;
+    var actualY = 0;
+
+    var isSandResting = false;
+
+    var actualNode = map.nodes.Where(n => n.x == actualX && n.y == actualY).First();
+    actualNode.obj = 'o';
+
+    var nextSandSpot = map.nodes.Where(n => n.x == actualX && n.y == actualY + 1).FirstOrDefault();
+
+    while(!isSandResting)
+    {
+        if (nextSandSpot != null)
+        {
+            if (nextSandSpot.obj == '.')
+            {
+                actualNode.obj = '.';
+                actualNode     = map.nodes.Where(n => n.x == nextSandSpot.x && n.y == nextSandSpot.y).First();
+                actualNode.obj = 'o';
+
+                nextSandSpot = map.nodes.Where(n => n.x == nextSandSpot.x && n.y == nextSandSpot.y + 1).FirstOrDefault();
+            }
+            else
+            {
+                var leftSandSpot  = map.nodes.Where(n => n.x == nextSandSpot.x - 1 && n.y == nextSandSpot.y + 1).FirstOrDefault();
+                var rightSandSpot = map.nodes.Where(n => n.x == nextSandSpot.x + 1 && n.y == nextSandSpot.y + 1).FirstOrDefault();
+
+                if (leftSandSpot == null)
+                {
+                    return (true, map);
+                }
+                else if (leftSandSpot.obj == '.')
+                {
+                    actualNode.obj = '.';
+                    actualNode     = map.nodes.Where(n => n.x == nextSandSpot.x && n.y == nextSandSpot.y).First();
+                    actualNode.obj = 'o';
+    
+                    nextSandSpot = map.nodes.Where(n => n.x == nextSandSpot.x && n.y == nextSandSpot.y + 1).FirstOrDefault();
+                }
+            }
+        }
+        else 
+        {
+            actualNode.obj = '.';
+            return (true, map);
         }
     }
 }
